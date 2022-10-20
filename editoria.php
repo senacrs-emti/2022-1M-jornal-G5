03f401a860ab4ce380726fd3a34934da61c8e337
artur5anto5 <artur2006santos@gmail.com>
2022-10-20 09:10:58 -0300

    editoria
    fazendo

--- editoria.php
+++ editoria.php
@@ -2,7 +2,18 @@
     include_once './includes/_header.php'
 ?>
 
-<main>editoria</main>
+<main>
+    <section>
+        <div class="noticias">
+            <img src="img/Verde.png">
+            <h3>Lorem ipsum dolor sit</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam voluptates quis modi voluptas! Eveniet nobis saepe autem exercitationem ullam itaque, iste cupiditate asperiores velit! Explicabo nisi voluptates cupiditate ea! Aperiam.</p>
+        </div>
+        <div class="anuncios">
+            <img src="img/Verde.png">
+        </div>
+    </section>
+</main>
 
 <?php
     include_once './includes/_footer.php'
